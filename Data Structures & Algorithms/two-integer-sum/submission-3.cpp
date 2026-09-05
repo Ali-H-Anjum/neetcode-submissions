e2faa22366480f3b2d16f081b1d15f38ce1846c2
Ali Anjum <ali.26.anjum@gmail.com>
2026-09-05 04:26:05 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> num_index;
+        std::size_t len = nums.size(); 
+
+        num_index.reserve(len);
+        for (std::size_t i = 0; i < len; ++i) {
+            int num = nums[i];
+            int diff = target - num;
+            if (num_index.contains(diff)) {
+                return {num_index[diff], static_cast<int>(i)};
+            } else {
+                num_index[num] = i;
+            }
+        }
+        return {};
+    }
+};
