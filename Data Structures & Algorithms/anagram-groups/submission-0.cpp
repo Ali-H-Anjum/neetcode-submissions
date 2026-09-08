18b73e21327a9eb4126dbc2958542faeed7f0c6d
Ali Anjum <ali.26.anjum@gmail.com>
2026-09-07 21:06:08 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> signatures;
+        vector<vector<string>> anagrams;
+
+        for (const string& str : strs) {
+            string signature = str;
+            sort(signature.begin(), signature.end());
+            signatures[signature].push_back(str);
+        }
+
+        for (auto& pair : signatures) {
+            anagrams.push_back(pair.second);
+        }
+        return anagrams;
+    }
+};
